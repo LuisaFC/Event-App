5c0cede11b3a739ce86e8bbb7b29561693c8f7a3
LuisaFC <luisabsb@gmail.com>
2021-11-30 16:52:24 -0300

    chore: add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Layout } from "systems/Core";
 
 export default function Home() {
-  return <Layout title="Events App">Hello World</Layout>
+  return <Layout title="Events App"><div className='text-indigo-500 p-10 bg-indigo-100'> Hello World</div></Layout>
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      ...colors,
+      gray: colors.blueGray,
+      primary: colors['indigo'],
+      secondary: colors['teal'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
